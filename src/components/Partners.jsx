297431432d8f6cdbf35f6f64479a09484118eab3
LaySopanha <panhalay69420@gmail.com>
2026-01-25 13:05:24 +0700

    partner logo replace

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import './Partners.css';
 
 const Partners = () => {
-    // Repeated logos for demonstration as per screenshot
-    const partners = [1, 2, 3, 4];
+    const partners = [
+        {
+            id: 1,
+            image: '/partner/ChatGPT_Image_Jan_23__2026__08_04_52_PM-removebg-preview.png',
+            alt: 'Partner 1'
+        },
+        {
+            id: 2,
+            image: '/partner/image_2026-01-23_11-26-02-removebg-preview.png',
+            alt: 'Partner 2'
+        },
+        {
+            id: 3,
+            image: '/partner/logo-BQHaJWnx.svg',
+            alt: 'Partner 3'
+        }
+    ];
 
     return (
         <section className="partners-section">
             <h2 className="partners-title">Наши Партнеры</h2>
             <div className="partners-grid">
-                {partners.map((id) => (
-                    <div key={id} className="partner-logo">
-                        <img src="/images/logoipisum.png" alt="Partner Logo" />
+                {partners.map((partner) => (
+                    <div key={partner.id} className="partner-logo">
+                        <img src={partner.image} alt={partner.alt} />
                     </div>
                 ))}
             </div>
